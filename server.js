a8e525aba7ee032f4c730ac3c884c962981ee3a3
Ethan Haynes <ethanhaynes@Ethans-MacBook-Pro.local>
2017-04-16 18:06:38 -0500

    fixed node-sass

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-//import sassMiddleware from 'node-sass-middleware'
 import path from 'path'
 import express from 'express'
 import config from './config'
 import apiRouter from './api'
 import serverRender from './serverRender'
 
-
 const server = express()
-// server.use( sassMiddleware({
-//   src: path.join( __dirname, 'sass' ),
-//   dest: path.join( __dirname, 'public' ),
-//   debug: true,
-//   outputStyle: 'compressed',
-//   prefix:  '/prefix'
-// }))
 
 server.set( 'view engine', 'ejs' )
 
